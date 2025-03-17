99dfc6bb68477355182190b496302cdb6c727184
haziqhaikal01 <haziqmuhammad01@gmail.com>
2025-03-17 12:49:22 +0000

    quick

--- react-frontend/src/main.tsx
+++ react-frontend/src/main.tsx
@@ -1,10 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Test from "./Test";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
-import Effect from "./practice/Useeffectpractice";
-import State from "./practice/Usestatepractice";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
